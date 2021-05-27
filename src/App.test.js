07a866836b3230d4c9a50dd0cab299d3af4f1bae
tiethan <rredhead@reasononeinc.com>
2021-05-27 15:21:42 -0400

    Added a unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Encounter Builder" header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Encounter Builder/i);
   expect(linkElement).toBeInTheDocument();
 });
